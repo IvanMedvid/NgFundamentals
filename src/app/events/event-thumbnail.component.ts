e637a83fd411ad56d7f84c2c92a8735870f4a06c
Ivan Medvid <medjo21@gmail.com>
2019-01-11 11:55:41 +0200

    Fix styling

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,7 +2,14 @@ import { Component, Input} from '@angular/core'
 
 @Component({
     selector : 'event-thumbnail',
-    templateUrl : 'event-thumbnail.component.html'
+    templateUrl : 'event-thumbnail.component.html', 
+    styles : [
+        `
+        .thumbnail { min-height: 210px; }
+        .pad-left { margin: 10px; }
+        .well div { color: #bbb; }
+        `
+    ]
 })
 
 export class EventThumbnailComponent{
