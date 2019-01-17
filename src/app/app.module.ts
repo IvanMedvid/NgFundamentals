f5224c82d7e1c715921cff3d4e128d5a02b28898
Ivan Medvid <Ivan_Medvid@epam.com>
2019-01-17 17:14:17 +0200

    Add auth form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
 import { NavBarComponent } from './nav/nav-bar.component';
 import { NotFoundComponent } from './errors/not-found.component';
 import { EventsAppComponent } from './events-app.component';
+import { AuthService } from './user/auth.service';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { EventsAppComponent } from './events-app.component';
     EventService,
     EventRouteActivator,
     EventListResolver,
+    AuthService,
     {provide: 'canDeactivateCreateEvent', useValue: checkDirtyState }
    ],
   bootstrap: [
