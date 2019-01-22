1502d3b0dd89b5d60fee61c37d26dea9c75385f9
Ivan Medvid <Ivan_Medvid@epam.com>
2019-01-22 12:52:45 +0200

    Use input set

--- src/app/events/event-details/upvote.component.ts
+++ src/app/events/event-details/upvote.component.ts
@@ -8,8 +8,11 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 export class UpvoteComponent{
     @Input() count: number;
-    @Input() voted: boolean;
+    @Input() set voted(val){
+        this.iconColor = val ? 'red' : 'white';
+    };
     @Output() vote = new EventEmitter();
+    iconColor: string;
 
     onClick(){
         this.vote.emit({});
